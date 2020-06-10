89e19bd0b3204a972a3600fb6349a5651510cce9
Juan Aleman <juan.j.aleman@gmail.com>
2020-06-09 19:33:20 -0500

    took out auth functions from userRouter and refactored them into restricted-middleware file

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -50,15 +50,6 @@ router.post("/login", (req, res) => {
       });
 });
 
-router.get("/users", (req, res) => {
-  Users.find()
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
-});
 
 function genToken(user) {
   const payload = {

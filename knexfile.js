88e69807897d2e0cd2e6436908a9ae2299f07df3
Juan Aleman <juan.j.aleman@gmail.com>
2020-06-17 19:47:46 -0500

    added testing to auth-router

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,16 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    client: "postgresql",
-    connection: {
-      database: "postgres",
-      user: "postgres",
-      password: "Ganesh23" || "12345",
-      host: "127.0.0.1",
-      port: 5432,
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    // client: "postgresql",
+    // connection: {
+    //   database: "postgres",
+    //   user: "postgres",
+    //   password: "Ganesh23" || "12345",
+    //   host: "127.0.0.1",
+    //   port: 5432,
+    // },
     migrations: {
       directory: "./data/migrations",
     },
